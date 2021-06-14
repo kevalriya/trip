7d4563c0eacba1c869d79b207ef2d6c493b1f1c4
Akshit <akshit@Akshits-iMac.local>
2021-06-14 14:01:40 -0400

    Update table name

--- app/Tab.php
+++ app/Tab.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Tab extends Model
 {
     //
-    protected $table = "tabs";
+    protected $table = "PAGE_NOTES";
     protected $primaryKey = 'id';
     protected $fillable = ['id', 'title', 'description'];
 }
