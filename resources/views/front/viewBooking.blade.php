0d66548d9c4ba4dbb43a9e2a42b74ee0af103e0d
Akshit Patel <aksh1150@gmail.com>
2021-06-27 00:04:13 -0400

    All fixed

--- resources/views/front/viewBooking.blade.php
+++ resources/views/front/viewBooking.blade.php
@@ -256,18 +256,11 @@ $ActiveSide='booking';
 <script>
 function printDiv() 
 {
-
   var divToPrint=document.getElementById('DivIdToPrint');
-
   var newWin=window.open('','Print-Window');
-
   newWin.document.open();
-
   newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
-
   newWin.document.close();
-
   setTimeout(function(){newWin.close();},10);
-
 }
 </script>
